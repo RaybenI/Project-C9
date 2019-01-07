c5100132e2b907e708ef97c40c36265c2733ceb9
RaybenI <44508784+RaybenI@users.noreply.github.com>
2019-01-07 12:23:03 -0800

    added new databases

---
+++ login2.php
@@ -0,0 +1,9 @@
+<?php
+    $host = "127.0.0.1";
+    $user = "rayben";                     
+    $pass = "";                                  
+    $db = "tigers";                                
+    $port = 3306;                                
+    ?>
+    
+<?php
---
+++ login3.php
@@ -0,0 +1,9 @@
+<?php
+    $host = "127.0.0.1";
+    $user = "rayben";                     
+    $pass = "";                                  
+    $db = "rhinos";                                
+    $port = 3306;                                
+    ?>
+    
+<?php
